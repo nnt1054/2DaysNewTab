44da4c0139fa0e5a20296ea2e60889c5ea51450d
Joshua Zeitsoff <jzeitsoff@berkely.edu>
2016-11-05 00:37:55 -0700

    Trying found

--- test_auth.js
+++ test_auth.js
@@ -6,13 +6,10 @@
   </head>
   <body>
     <p>Say hello using the People API.</p>
-
-    <!--Add buttons to initiate auth sequence and sign out-->
+<div id="content"></div>
+<!--Add buttons to initiate auth sequence and sign out-->
     <button id="authorize-button" style="display: none;">Authorize</button>
     <button id="signout-button" style="display: none;">Sign Out</button>
-
-    <div id="content"></div>
-
     <script type="text/javascript">
       // Enter an API key from the Google API Console:
       //   https://console.developers.google.com/apis/credentials
@@ -25,7 +22,7 @@
       //   https://console.developers.google.com/apis/credentials?project=_
       // In your API Console project, add a JavaScript origin that corresponds
       //   to the domain where you will be running the script.
-      var clientId = 993464514830-bs0hqjvqe1t4p1ksskjv6i3k506vptuc.apps.googleusercontent.com;
+      var clientId = 993464514830-bs0hqjvqe1t4p1ksskjv6i3k506vptuc.apps.googleusercontent.com,;
       // Enter one or more authorization scopes. Refer to the documentation for
       // the API or https://developers.google.com/people/v1/how-tos/authorizing
       // for details.
@@ -45,40 +42,39 @@
         }).then(function () {
           // Listen for sign-in state changes.
           gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-          // Handle the initial sign-in state.
-          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-          authorizeButton.onclick = handleAuthClick;
-          signoutButton.onclick = handleSignoutClick;
-        });
-      }
-      function updateSigninStatus(isSignedIn) {
-        if (isSignedIn) {
-          authorizeButton.style.display = 'none';
-          signoutButton.style.display = 'block';
-          makeApiCall();
-        } else {
-          authorizeButton.style.display = 'block';
-          signoutButton.style.display = 'none';
-        }
-      }
-      function handleAuthClick(event) {
-        gapi.auth2.getAuthInstance().signIn();
-      }
-      function handleSignoutClick(event) {
-        gapi.auth2.getAuthInstance().signOut();
-      }
-      // Load the API and make an API call.  Display the results on the screen.
-      function makeApiCall() {
-        gapi.client.people.people.get({
-          resourceName: 'people/me'
-        }).then(function(resp) {
-          var p = document.createElement('p');
-          var name = resp.result.names[0].givenName;
-          p.appendChild(document.createTextNode('Hello, '+name+'!'));
-          document.getElementById('content').appendChild(p);
-        });
-      }
-    </script>
+                    // Handle the initial sign-in state.
+                    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+                    authorizeButton.onclick = handleAuthClick;
+                    signoutButton.onclick = handleSignoutClick;
+                  });
+                }
+                function updateSigninStatus(isSignedIn) {
+                  if (isSignedIn) {
+                    authorizeButton.style.display = 'none';
+                    signoutButton.style.display = 'block';
+                    makeApiCall();
+                  } else {
+                    authorizeButton.style.display = 'block';
+                    signoutButton.style.display = 'none';
+                  }
+                }
+                function handleAuthClick(event) {
+                  gapi.auth2.getAuthInstance().signIn();
+                }
+                function handleSignoutClick(event) {
+                  gapi.auth2.getAuthInstance().signOut();
+                }
+                // Load the API and make an API call.  Display the results on the screen.
+                function makeApiCall() {
+                  gapi.client.people.people.get({
+                    resourceName: 'people/me'
+                  }).then(function(resp) {
+                    var p = document.createElement('p');
+                    var name = resp.result.names[0].givenName;
+                    p.appendChild(document.createTextNode('Hello, '+name+'!'));
+                    document.getElementById('content').appendChild(p);
+                  });
+                  </script>
     <script async defer src="https://apis.google.com/js/api.js"
       onload="this.onload=function(){};handleClientLoad()"
       onreadystatechange="if (this.readyState === 'complete') this.onload()">
