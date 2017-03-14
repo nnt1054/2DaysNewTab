148906b9096f5300a068a6353a9ae95a0cbdb9ad
Neil Toledo <ntoledo@berkeley.edu>
2017-03-14 01:26:41 -0700

    setup calendar layout

---
+++ js/layout.js
@@ -0,0 +1,44 @@
+var tlist = document.getElementById("time-list");
+var todayList = document.getElementById("today-list");
+var tommorowList = document.getElementById("tomorrow-list");
+
+function makeHourLabel(hour, suffix) {
+    var li = document.createElement("li");
+    var div = document.createElement("div");
+    div.innerHTML = hour + " " + suffix;
+    li.className = "hour";
+    li.appendChild(div);
+    tlist.appendChild(li);
+}
+
+function setLabels() {
+    var am = "AM", pm = "PM";
+    makeHourLabel(12, am);
+    for (var i = 1; i < 12; i++) {
+      makeHourLabel(i, am);
+    }
+    makeHourLabel(12, pm);
+    for (var i = 1; i < 12; i++) {
+      makeHourLabel(i, pm);
+    }
+}
+
+function makeEventSection(list) {
+  var li = document.createElement("li");
+  var div = document.createElement("div");
+  li.className = "hour";
+  li.appendChild(div);
+  list.appendChild(li);
+}
+
+function setGrid() {
+  for (var i = 0; i < 24; i++) {
+    makeEventSection(todayList);
+  }
+  for (var i = 0; i < 24; i++) {
+    makeEventSection(tommorowList);
+  }
+}
+
+setLabels();
+setGrid();
